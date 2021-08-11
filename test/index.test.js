3a2a4a6abfdf901fac9ad1be9ecec73d98409fe1
suzume <johnstevemap@gmail.com>
2021-08-11 13:08:52 +0630

    fixing bugs

--- test/index.test.js
+++ test/index.test.js
@@ -5,6 +5,10 @@ successMessage="Testing Burmese Braille Muthit Converter with syllable breaks is
 if(burmeseBrailleConverter("ပွင့် ဖူး ကြ ချိန် အ မှန် ၊ ဝ ဿန္တ လေ ချို ဖျန်း ။")=="⠏⠜⠌⠄⠂ ⠘⠥⠆ ⠅⠢ ⠨⠔⠊⠝⠄ ⠣ ⠍⠭⠝⠄ ⠒ ⠺ ⠰⠹⠝⠤⠞ ⠱⠇ ⠨⠔⠊⠑ ⠘⠔⠝⠄⠆ ⠲"){
 	console.log(successMessage)
 }
-if(burmeseBrailleConverter("၁၉၂၀ ပြည့် ကျောင်း သား သ ပိတ် ကြီး တွင် ဦး ဖိုး ကျား သည် ထင် ရှား သော ခေါင်း ‌ဆောင် တစ် ဦး ဖြစ် သည် ။")=="⠼⠁⠊⠃⠚ ⠏⠢⠷⠄⠂ ⠱⠱⠔⠁⠌⠄⠆ ⠹⠁⠆ ⠹ ⠏⠊⠞⠄ ⠅⠢⠪⠆ ⠞⠜⠌⠄ ⠰⠑⠪⠆ ⠘⠊⠑⠆ ⠅⠔⠁⠆ ⠹⠷⠄ ⠚⠌⠄ ⠗⠭⠁⠆ ⠱⠹⠁ ⠱⠨⠰⠁⠌⠄⠆  ⠱⠖⠁⠌⠄ ⠞⠎⠄ ⠰⠑⠪⠆ ⠘⠢⠎⠄ ⠹⠷⠄ ⠲"){
+
+if(burmeseBrailleConverter("၁၉၂၀ ပြည့် ကျောင်း သား သ ပိတ် ကြီး တွင် ဦး ဖိုး ကျား သည် ထင် ရှား သော ခေါင်း ‌ဆောင် တစ် ဦး ဖြစ် သည် ။")=="⠼⠁⠊⠃⠚ ⠏⠢⠷⠄⠂ ⠱⠅⠔⠁⠌⠄⠆ ⠹⠁⠆ ⠹ ⠏⠊⠞⠄ ⠅⠢⠪⠆ ⠞⠜⠌⠄ ⠰⠑⠪⠆ ⠘⠊⠑⠆ ⠅⠔⠁⠆ ⠹⠷⠄ ⠚⠌⠄ ⠗⠭⠁⠆ ⠱⠹⠁ ⠱⠨⠰⠁⠌⠄⠆  ⠱⠖⠁⠌⠄ ⠞⠎⠄ ⠰⠑⠪⠆ ⠘⠢⠎⠄ ⠹⠷⠄ ⠲"){
+	console.log(successMessage)
+}
+if(burmeseBrailleConverter("အမြွှာ")=="⠣⠍⠢⠜⠭⠁"){
 	console.log(successMessage)
 }
