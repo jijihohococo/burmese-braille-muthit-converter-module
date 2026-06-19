72324b4509276015ed3e268b43eed3d1c09f54bc
jijihohococo <jijihohococo@gmail.com>
2026-06-20 02:11:20 +0900

    Refactor Burmese Braille Converter: Improve code readability and maintainability
    - Simplified the logic for handling special cases in the conversion process.
    - Consolidated repetitive code blocks for better clarity.
    - Enhanced the test cases for better validation of the converter's functionality.
    - Ensured consistent formatting and spacing in the codebase.

--- test/index.test.js
+++ test/index.test.js
@@ -1,22 +1,22 @@
 const burmeseBrailleConverter = require('../lib');
 
-successMessage="Testing Burmese Braille Muthit Converter with syllable breaks is passed";
-normalSuccessMessage="Testing Burmese Braille Muthit Converter without syllable breaks is passed";
+successMessage = "Testing Burmese Braille Muthit Converter with syllable breaks is passed";
+normalSuccessMessage = "Testing Burmese Braille Muthit Converter without syllable breaks is passed";
 
-if(burmeseBrailleConverter("ပွင့် ဖူး ကြ ချိန် အ မှန် ၊ ဝ ဿန္တ လေ ချို ဖျန်း ။")=="⠏⠜⠌⠄⠂ ⠘⠥⠆ ⠅⠢ ⠨⠔⠊⠝⠄ ⠣ ⠍⠭⠝⠄ ⠒ ⠺ ⠰⠹⠝⠤⠞ ⠱⠇ ⠨⠔⠊⠑ ⠘⠔⠝⠄⠆ ⠲"){
+if(burmeseBrailleConverter("ပွင့် ဖူး ကြ ချိန် အ မှန် ၊ ဝ ဿန္တ လေ ချို ဖျန်း ။") == "⠏⠜⠌⠄⠂ ⠘⠥⠆ ⠅⠢ ⠨⠔⠊⠝⠄ ⠣ ⠍⠭⠝⠄ ⠒ ⠺ ⠰⠹⠝⠤⠞ ⠱⠇ ⠨⠔⠊⠑ ⠘⠔⠝⠄⠆ ⠲"){
 	console.log(successMessage)
 }
 
-if(burmeseBrailleConverter("၁၉၂၀ ပြည့် ကျောင်း သား သ ပိတ် ကြီး တွင် ဦး ဖိုး ကျား သည် ထင် ရှား သော ခေါင်း ‌ဆောင် တစ် ဦး ဖြစ် သည် ။")=="⠼⠁⠊⠃⠚ ⠏⠢⠷⠄⠂ ⠱⠅⠔⠁⠌⠄⠆ ⠹⠁⠆ ⠹ ⠏⠊⠞⠄ ⠅⠢⠪⠆ ⠞⠜⠌⠄ ⠰⠑⠪⠆ ⠘⠊⠑⠆ ⠅⠔⠁⠆ ⠹⠷⠄ ⠚⠌⠄ ⠗⠭⠁⠆ ⠱⠹⠁ ⠱⠨⠰⠁⠌⠄⠆  ⠱⠖⠁⠌⠄ ⠞⠎⠄ ⠰⠑⠪⠆ ⠘⠢⠎⠄ ⠹⠷⠄ ⠲"){
+if(burmeseBrailleConverter("၁၉၂၀ ပြည့် ကျောင်း သား သ ပိတ် ကြီး တွင် ဦး ဖိုး ကျား သည် ထင် ရှား သော ခေါင်း ‌ဆောင် တစ် ဦး ဖြစ် သည် ။") == "⠼⠁⠊⠃⠚ ⠏⠢⠷⠄⠂ ⠱⠅⠔⠁⠌⠄⠆ ⠹⠁⠆ ⠹ ⠏⠊⠞⠄ ⠅⠢⠪⠆ ⠞⠜⠌⠄ ⠰⠑⠪⠆ ⠘⠊⠑⠆ ⠅⠔⠁⠆ ⠹⠷⠄ ⠚⠌⠄ ⠗⠭⠁⠆ ⠱⠹⠁ ⠱⠨⠰⠁⠌⠄⠆  ⠱⠖⠁⠌⠄ ⠞⠎⠄ ⠰⠑⠪⠆ ⠘⠢⠎⠄ ⠹⠷⠄ ⠲"){
 	console.log(successMessage)
 }
-if(burmeseBrailleConverter("အမြွှာ")=="⠣⠍⠢⠜⠭⠁"){
+if(burmeseBrailleConverter("အမြွှာ") == "⠣⠍⠢⠜⠭⠁"){
 	console.log(normalSuccessMessage)
 }
 
-if(burmeseBrailleConverter("ကြွေ")=="⠱⠅⠢⠜"){
+if(burmeseBrailleConverter("ကြွေ") == "⠱⠅⠢⠜"){
 	console.log(normalSuccessMessage)
 }
-if(burmeseBrailleConverter("ရွှေမြန်မာများရွှေ့ပြောင်းကြခြင်း")=="⠱⠗⠜⠭⠍⠢⠝⠄⠍⠁⠍⠔⠁⠆⠱⠗⠜⠭⠂⠱⠏⠢⠁⠌⠄⠆⠅⠢⠨⠢⠌⠄⠆"){
+if(burmeseBrailleConverter("ရွှေမြန်မာများရွှေ့ပြောင်းကြခြင်း") == "⠱⠗⠜⠭⠍⠢⠝⠄⠍⠁⠍⠔⠁⠆⠱⠗⠜⠭⠂⠱⠏⠢⠁⠌⠄⠆⠅⠢⠨⠢⠌⠄⠆"){
 	console.log(normalSuccessMessage)
 }
